24bf243bf3ad8d1fa5f44cb864bfe95eec5e0189
cunguyendev <cunguyen.dev@gmail.com>
2021-01-22 15:26:23 +0700

    Add the base app.

---
+++ src/scripts/App.vue
@@ -0,0 +1,21 @@
+<template lang="html">
+  <p class="hello">Hello {{ bundler }}</p>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  data() {
+    return {
+      bundler: 'Parcel',
+    };
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.hello {
+  color: green;
+}
+</style>
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,7 @@
-console.log('Hello world! Parcel - The app');
+import Vue from 'vue';
+import App from './App.vue';
+
+new Vue({
+  template: '<App/>',
+  components: { App },
+}).$mount('#header');
