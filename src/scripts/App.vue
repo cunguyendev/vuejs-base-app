566b920d4bbeb7f38a740f4575b4dc1b18d1c279
cunguyendev <cunguyen.dev@gmail.com>
2021-01-22 19:48:34 +0700

    Add the base content for app.

--- src/scripts/App.vue
+++ src/scripts/App.vue
@@ -1,14 +1,35 @@
 <template lang="html">
-  <p class="hello">Hello {{ bundler }}</p>
+  <table>
+    <thead>
+      <tr>
+        <th>No</th>
+        <th>Name</th>
+        <th>Class</th>
+        <th>Phone</th>
+        <th>Address</th>
+      </tr>
+    </thead>
+    <tbody v-for="(user, index) in users" :key="index" class="data-repeat">
+      <tr>
+        <td>{{ index + 1 }}</td>
+        <td>{{ user.name }}</td>
+        <td>{{ user.class }}</td>
+        <td>{{ user.phone }}</td>
+        <td>{{ user.address }}</td>
+      </tr>
+    </tbody>
+  </table>
 </template>
 
-<script lang="ts">
+<script lang="js">
 import Vue from 'vue';
+import users from './user';
 
 export default Vue.extend({
   data() {
     return {
       bundler: 'Parcel',
+      users: users,
     };
   },
 });
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,4 +4,4 @@ import App from './App.vue';
 new Vue({
   template: '<App/>',
   components: { App },
-}).$mount('#header');
+}).$mount('#user-list');
